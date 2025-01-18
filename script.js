c3a16fc896ec1d27719620ff89b800fdde21f8dd
Nick <36767444+njcos@users.noreply.github.com>
2025-01-17 22:44:39 -0500

    started the layout of the calculator

--- script.js
+++ script.js
@@ -1,5 +1,18 @@
-const body = document.querySelector('body')
-const boilerplate = document.createElement('h1')
+const numPanel = document.querySelector(".nums")
 
-boilerplate.textContent="connected"
-body.appendChild(boilerplate)
+function numberButtons () {
+    
+    for(let i = 1; i <= 4; i++) {
+        const numRows = document.createElement('div')
+        numRows.classList.add('numRows')
+        numPanel.appendChild(numRows)
+        for(let r = 1; r <=3; r++) {
+            const numButtons = document.createElement('button')
+            numButtons.classList.add('numButtons')
+            numRows.appendChild(numButtons)
+        }
+
+    }
+}
+ 
+numberButtons()
