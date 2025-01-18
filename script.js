54bccd606245b2d3a0d3b53ef0c50ade65cf91fc
Nick <36767444+njcos@users.noreply.github.com>
2025-01-18 13:00:26 -0500

    Working on layout

--- script.js
+++ script.js
@@ -1,5 +1,8 @@
 const numPanel = document.querySelector(".nums")
+const operatorPanel = document.querySelector(".operators")
 
+
+// CREATING THE NUMBER BUTTONS  
 function numberButtons () {
     
     for(let i = 1; i <= 4; i++) {
@@ -9,6 +12,7 @@ function numberButtons () {
         for(let r = 1; r <=3; r++) {
             const numButtons = document.createElement('button')
             numButtons.classList.add('numButtons')
+            numButtons.textContent='1'
             numRows.appendChild(numButtons)
         }
 
@@ -16,3 +20,21 @@ function numberButtons () {
 }
  
 numberButtons()
+
+//CREATE OPERATOR NUMBERS
+
+function opButtons () {
+    for(let i = 1; i <= 2; i++) {
+        const operatorRow = document.createElement('div')
+        operatorRow.classList.add('operatorRow')
+        operatorPanel.appendChild(operatorRow)
+        for(let r = 1; r <= 2; r++) {
+            const operatorButtons = document.createElement('button')
+            operatorButtons.classList.add('operatorButtons')
+            operatorButtons.textContent='+'
+            operatorRow.appendChild(operatorButtons)
+        }
+    }
+}
+
+opButtons()
